1757c261e2394c4fe3cdc267ada55b227bd0c297
Elsa Beesta <elishabista761@gmail.com>
2018-07-31 07:53:02 +0545

    neha

---
+++ Substract.java
@@ -0,0 +1,7 @@
+public class Substract()
+{
+	public class static void main(String[] args)
+	int a,b;
+	System.out.println("Substract=a-b");
+
+}
---
+++ Sum.java
@@ -0,0 +1,6 @@
+public class Sum{
+	public class static void main(){
+		int a,b;
+		System.out.println("Sum=a+b");
+	}
+}
