14554c8534510e1f256d5146617cbf3cb473115a
Elsa Beesta <elishabista761@gmail.com>
2018-07-31 07:19:23 +0545

    darling

---
+++ Elsa.java
@@ -0,0 +1,16 @@
+public class Elsa{
+	public  static void main(String[] args
+	for(i=0;i<10;i++)
+	{
+	if(i/2==0)
+	System.out.println("Even")
+	else
+		System.out.println("Odd")
+
+
+	}
+
+
+	
+	
+}
